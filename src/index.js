66693f9db6ce328b7e33e1a74c37b7ee78631b0b
Ojasvita Dahare <dahare.ojasvita@gmail.com>
2023-06-29 15:25:22 +0530

    Adding project files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import App from './Containers/App';
+import 'tachyons';
+
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <div>
+  <App/>
+   
+   
+   </div>
   </React.StrictMode>
 );
 
